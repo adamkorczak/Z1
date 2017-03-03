1bec41854ea4501a08da7507be2200fdca525f99
adamkorczak <a.korczak93@gmail.com>
2017-03-03 14:15:36 +0100

    Add files via upload

--- header.hpp
+++ header.hpp
@@ -5,5 +5,7 @@
 using namespace std;
 
 string Load_and_write(string name);
+string Find_vowels_and_change(string name);
+
 
 #endif // header
--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@ int main()
     string name;
 
     name = Load_and_write(name);
-
     cout << name << endl;
+
+    name = Find_vowels_and_change(name);
+    cout << name << endl;
+
     return 0;
 }
