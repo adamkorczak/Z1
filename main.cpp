bf3fd6e1a1e1f1157b271b48bfececfa04e81103
adamkorczak <a.korczak93@gmail.com>
2017-03-03 13:45:18 +0100

    Add files via upload
    First step

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "header.hpp"
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    name = Load_and_write(name);
+
+    cout << name << endl;
+    return 0;
+}
